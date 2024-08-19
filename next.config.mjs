6c7bb59c5a98a4a37bbc68fc226025253e9b85a0
박영진 <dud_wls_95@naver.com>
2024-08-19 09:26:52 +0900

    Chore: PWA 설치 및 프로젝트 생성

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const prod = process.env.NODE_ENV === 'production';
+
+const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: prod ? false : true
+});
+
+const nextConfig = withPWA({
+  // next config
+});
+module.exports = nextConfig;
 
 export default nextConfig;
