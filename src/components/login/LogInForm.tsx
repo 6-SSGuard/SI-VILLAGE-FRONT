1190fd6268f762c2128f859787acc2084e64c1e4
박영진 <dud_wls_95@naver.com>
2024-08-21 09:32:35 +0900

    Feat: 회원가입 퍼블리싱 추가

--- src/components/login/LogInForm.tsx
+++ src/components/login/LogInForm.tsx
@@ -1,15 +1,17 @@
-import { Button } from "../ui/button"
-import { Input } from "../ui/input"
+import React from "react";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 function LogInForm() {
-
   return (
-    <form>
-      <Input type="email" name="email" placeholder="Email" />
-      <Input type="password" name="password" placeholder="Password" />
-      <Button type="submit">Sign Up</Button>
-    </form>
-  )
+    <>
+      <form>
+        <Input type="email" name="email" placeholder="Email" />
+        <Input type="password" name="password" placeholder="Password" />
+        <Button type="submit">Sign Up</Button>
+      </form>
+    </>
+  );
 }
 
-export default LogInForm
+export default LogInForm;
