bfe6966f9bf8bfbadcba3be8d3e3bb8647dbda82
snow <cksdn0716@naver.com>
2024-08-22 17:41:44 +0900

    Feat: 카테고리 컴포넌트 구현중

--- src/app/main/initMain/page.tsx
+++ src/app/main/initMain/page.tsx
@@ -1,10 +1,17 @@
 import MainImage from '@/components/main/Home_Image';
+import ButtonList from '@/components/main/ButtonList';
+
 function maininit() {
 
     return (
+
+        <main>
         <MainImage>
             
         </MainImage>
+
+        <ButtonList />
+        </main>
     )
 }
 
---
+++ src/components/main/ButtonList.tsx
@@ -0,0 +1,16 @@
+import { Button } from "../ui/button";
+
+function ButtonList() {
+
+    return (
+        <main>
+        
+        <div className="m-8">
+        <Button className="bounded-full pw-4 w-12 h-12 py-2 bg-blue-300"> dadad </Button>
+
+        </div>
+        </main>
+    )
+}
+
+export default ButtonList
