661194749e9f337de235dbc3e47dd981d2e2461d
snow <cksdn0716@naver.com>
2024-08-26 15:58:50 +0900

    Feat: 배너 컴포넌트 퍼블리싱 진행중

--- src/app/main/initMain/page.tsx
+++ src/app/main/initMain/page.tsx
@@ -1,19 +1,22 @@
 import MainImage from '@/components/main/HomeImage';
 import ButtonList from '@/components/main/Category';
+import Eventbanner from '@/components/main/EventBanner';
 
 function maininit() {
 
     return (
 
         <main>
-
-            <div
-            >
+        <div style = {{
+            overflowY:"auto"
+        }}>
         <MainImage>
             
         </MainImage>
 
         <ButtonList />
+
+        <Eventbanner/>
         </div>
         </main>
     )
