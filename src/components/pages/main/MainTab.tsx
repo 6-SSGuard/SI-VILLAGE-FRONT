e343fddc27d23737342b9b16ae04a807a70c3fd8
박영진 <dud_wls_95@naver.com>
2024-08-29 09:29:58 +0900

    Fix: 오류수정

--- src/components/pages/main/MainTab.tsx
+++ src/components/pages/main/MainTab.tsx
@@ -6,13 +6,15 @@ function Tab() {
   return (
     <main>
       <Tabs className="w-full h-16 flex justify-center items-center">
-        <TabsList className=" scrollb mt-10 overflow-x-auto whitespace-nowrap scrollbar-hide pl-5 pr-5">
+        <TabsList className="mt-10 overflow-x-auto whitespace-nowrap scrollbar-hide pl-5 pr-5">
           <TabsTrigger className=" m1-6" value={"Home"}>
             Home
           </TabsTrigger>
-          <TabsTrigger className=" ml-6 " value={"Event"}>
-            Event
-          </TabsTrigger>
+          <Link href="/event">
+            <TabsTrigger className=" ml-6 " value={"Event"}>
+              Event
+            </TabsTrigger>
+          </Link>
           <Link href="/deal">
             <TabsTrigger className=" ml-6 " value={"Deal"}>
               Deal
