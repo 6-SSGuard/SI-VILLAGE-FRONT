1f1fbad4f3f2b2272f175cac48a6e0146f8b1cd6
박영진 <dud_wls_95@naver.com>
2024-09-13 11:15:48 +0900

    Fix: 빌드에러 해결

--- src/app/(store)/officialstore/page.tsx
+++ src/app/(store)/officialstore/page.tsx
@@ -1,12 +1,10 @@
 import StoreTab from '@/components/layouts/StoreTab';
-import ProductQnA from '@/components/pages/product/ProductQnA';
 import React from 'react';
 
 function page() {
   return (
     <main>
       <StoreTab />
-      <ProductQnA />
     </main>
   );
 }
