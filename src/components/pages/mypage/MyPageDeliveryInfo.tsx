6e201d2a5242eb65ed7722a78f2d61ef932392d5
박영진 <dud_wls_95@naver.com>
2024-09-16 21:43:16 +0900

    Fix: 마이페이지 컴포넌트화 더 세분화

--- src/components/pages/mypage/MyPageDeliveryInfo.tsx
+++ src/components/pages/mypage/MyPageDeliveryInfo.tsx
@@ -16,26 +16,24 @@ export default function MyPageDeliveryInfo() {
     { icon: <Delivery5Icon />, label: '배송완료', count: 0 },
   ];
   return (
-    <div>
-      <div className="bg-gray-100 p-4 rounded-md">
-        <h2 className="text-lg font-semibold mb-4">
-          {/* 주문 조회 링크로 연결 */}
+    <section className="bg-[#F8F8F8] p-[8px_0px_32px] rounded-md mb-4">
+      <header className="mb-4">
+        <h2 className="text-lg font-semibold px-4">
           <Link href="#" passHref>
             주문/배송 조회
           </Link>
         </h2>
-        <ul className="flex flex-row space-x-4 text-xs text-si-787878">
-          {/* 반복되는 배송 상태 컴포넌트 */}
-          {deliveryStatus.map((status, index) => (
-            <DeliveryStatusItem
-              key={index}
-              icon={status.icon}
-              label={status.label}
-              count={status.count}
-            />
-          ))}
-        </ul>
-      </div>
-    </div>
+      </header>
+      <ul className="flex flex-row justify-between text-xs text-si-787878 px-4">
+        {deliveryStatus.map((status, index) => (
+          <DeliveryStatusItem
+            key={index}
+            icon={status.icon}
+            label={status.label}
+            count={status.count}
+          />
+        ))}
+      </ul>
+    </section>
   );
 }
